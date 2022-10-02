a6337bd43c89ca5fcf87145f7caf226f577b7191
unknown <hieuproxd123456@gmail.com>
2022-10-02 18:54:39 +0700

    still in progress

--- simple_landing_page.js
+++ simple_landing_page.js
@@ -7,10 +7,12 @@
 
     contactBtn.onclick=()=>{
         modal.classList.add('modal-open');
+        document.body.style.overflow='hidden';
     }
     // modal-close
     function closeModal(){
         modal.classList.remove('modal-open');
+        document.body.style.overflow='auto';
     }
     
     closeBtn.onclick=closeModal;
@@ -21,9 +23,45 @@
     // modal-textarea
     let modalTextarea = document.querySelector('textarea')
     modalTextarea.oninput = ()=>{
-        modalTextarea.style.height=modalTextarea.scrollHeight+"px"
+        modalTextarea.style.height=modalTextarea.scrollHeight+"px";
     }
     
+// fixed-nav
+let fixedNav = document.getElementById('header');
+window.onscroll=()=>{
+    // document.body vs document.documentElement is for different browser
+    if(document.body.scrollTop>400||document.documentElement.scrollTop>400){
+        fixedNav.classList.add('fixed-nav');
+    }else{
+        fixedNav.classList.remove('fixed-nav');
+    }
+}
+
+// scroll-smooth when clicking anchor link
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// scroll-spy
+let activeLinks = document.querySelectorAll('.nav-link');
+let activeSections = document.querySelectorAll('section');
+let addActive = (link)=>{
+    activeLinks[link].classList.add('.active');
+}
+let removeActive = (link)=>{
+    activeLinks[link].classList.remove('.active');
+}
+let removeAllActive = (link)=>{
+    // convert DOM nodes into array and then loop through each element n remove them
+    [...Array(activeSections.length)].forEach((link)=>removeActive(link));
+}
+
 // swiper-review
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -38,6 +76,6 @@ var swiper = new Swiper(".mySwiper", {
     },
     autoplay: {
         enabled: true,
-        delay: 5000,
+        delay: 4000,
     },
   });
